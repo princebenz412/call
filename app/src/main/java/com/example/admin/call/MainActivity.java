5f2097ede673bbf8e26b355101115082f2dc2e2d
princebenz412 <princebenz412@gmail.com>
2016-12-09 16:25:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/admin/call/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.admin.call;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.call);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent CallIntent = new Intent(Intent.ACTION_CALL);
+                CallIntent.setData(Uri.parse("tel:= 191"));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                startActivity(CallIntent);
+            }
+        });
+
+    }
+}
